c723b54b95bc8a3b59df33a9d9ee8fb6890e235d
bnat1 <nwbaylon@gmail.com>
2020-07-31 21:39:12 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9fe687d7b05d2ca1d7fc.js"
+    "url": "webpack-runtime-f66bc5f37cb09f6c12d8.js"
   },
   {
     "url": "framework-a576ae5ab153fa4a7c27.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cbb702f30f310619326ae8f0e318acf6"
+    "revision": "701edc0491641a2018bf8020e293d387"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
